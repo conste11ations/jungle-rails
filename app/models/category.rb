fa4eee302db282ac658c0958b0d0d712d4b83783
Rachel Kat <rachelkat@gmail.com>
2020-05-28 19:51:31 +0000

    added validation so you cannot add the same category twice

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,12 @@ class Category < ActiveRecord::Base
 
   has_many :products
   validates :name, presence: true
+  validate :only_distinct_categories, on: :create
+
+  def only_distinct_categories
+    if Category.exists?(name: name)
+      errors.add(:base, 'This category already exists')
+    end
+  end
 
 end
