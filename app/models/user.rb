ef950fcfde91063689927b2bc229e71ab9fc3ff5
Rachel Kat <rachelkat@gmail.com>
2020-06-02 04:18:20 +0000

    refactor Users to have first and  last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates :name, :email, presence: true
+  validates :first_name, :last_name, :email, presence: true
 
   validates :email, uniqueness: {case_sensitive: false}
 
   validates :password, length: { minimum: 1 }, on: :create
 
   def self.authenticate_with_credentials(email, password)
-    
+
     user = User.find_by(email: email.strip)
     user && user.authenticate(password) ? user : nil
   end
