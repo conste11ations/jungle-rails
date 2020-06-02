2b0d0302125d411c54fb3249fc7b8a3d6796a2e5
Rachel Kat <rachelkat@gmail.com>
2020-06-02 04:06:27 +0000

    wrote tests for user_spec but need to refactor to have first last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :name, :email, presence: true
+
+  validates :email, uniqueness: {case_sensitive: false}
+
+  validates :password, length: { minimum: 1 }, on: :create
+
+  def self.authenticate_with_credentials(email, password)
+    
+    user = User.find_by(email: email.strip)
+    user && user.authenticate(password) ? user : nil
+  end
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,113 @@
+require 'rails_helper'
+
+  RSpec.describe User, type: :model do
+    describe 'Validations' do
+      subject {
+        described_class.new(
+          first_name: "First",
+          last_name: "Last",
+          email: "email@test.com",
+          password: "test",
+          password_confirmation: "test"
+        )}
+      it "is valid with valid attributes" do
+        subject.save
+        is_expected.to be_valid
+      end
+  
+      it "is not valid without a first name" do
+        subject.first_name = nil
+  
+        expect(subject).to_not be_valid
+        expect(subject.errors.full_messages).to include("First name can't be blank")
+      end
+  
+      it "is not valid without a last name" do
+        subject.last_name = nil
+  
+        expect(subject).to_not be_valid
+        expect(subject.errors.full_messages).to include("Last name can't be blank")
+      end
+  
+      it "is not valid without a email" do
+        subject.email = nil
+  
+        expect(subject).to_not be_valid
+        expect(subject.errors.full_messages).to include("Email can't be blank")
+      end
+  
+      it "is not valid without a password" do
+        subject.password = nil
+  
+        expect(subject).to_not be_valid
+        expect(subject.errors.full_messages).to include("Password can't be blank")
+      end
+  
+      it "is not valid without matching password and password confirmation" do
+        subject.password = "that"
+        subject.password_confirmation = "test"
+  
+        expect(subject).to_not be_valid
+        expect(subject.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+  
+      it "is not valid without a 4 character password" do
+        subject.password = "the"
+        subject.password_confirmation = "the"
+  
+        expect(subject).to_not be_valid
+        expect(subject.errors.full_messages).to include("Password is too short (minimum is 4 characters)")
+      end
+  
+      it "is not valid with a duplicate email" do
+        user = User.new(
+          first_name: "First",
+          last_name: "Last",
+          email: "Email@Test.com",
+          password: "test",
+          password_confirmation: "test"
+        )
+  
+        user.save
+  
+        expect(subject).to_not be_valid
+        expect(subject.errors.full_messages).to include("Email has already been taken")
+      end
+    end
+  
+    describe '.authenticate_with_credentials' do
+      subject {
+        described_class.new(
+          first_name: "First",
+          last_name: "Last",
+          email: "email@test.com",
+          password: "test",
+          password_confirmation: "test"
+        )}
+      it "is valid with existing email and matching password" do
+        subject.save
+  
+        expect(User.authenticate_with_credentials("email@test.com", "test")).to be_valid
+      end
+  
+      it "is not valide with incorrect password" do
+        subject.save
+  
+        login = User.authenticate_with_credentials("email@test.com", "text")
+  
+        expect(login).to be_nil
+      end
+  
+      it "is valid with an email with extra space" do
+        subject.save
+  
+        expect(User.authenticate_with_credentials(" email@test.com ", "test")).to be_valid
+      end
+  
+      it "is valid with an email not matching case" do
+        subject.save
+  
+        expect(User.authenticate_with_credentials("eMaIl@tEst.cOm", "test")).to be_valid
+      end
+    end
+  end
